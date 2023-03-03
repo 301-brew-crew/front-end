7a866dada7b186faf99c65b4b21af452e96b1dca
Leah Wilner <leahwilner1@gmail.com>
2023-03-02 20:01:37 -0600

    auth0 login working

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-  domain="dev-rfa4a3egya30fvin.us.auth0.com"
-  clientId="6EGzsLCqJWElyLmQ5BpleB9uCcqv2toM"
+  domain={process.env.REACT_APP_AUTH_DOMAIN}
+  clientId={process.env.REACT_APP_AUTH_CLIENTID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
