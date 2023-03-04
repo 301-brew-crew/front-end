ef08ba89fc450e2d9894900fbb101c9afdab3d20
Chris Vander Linden <cvl123abc@gmail.com>
2023-03-04 14:33:25 -0600

    Add GET to Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from 'axios';
 
 const Profile = () => {
+
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
+  axios.get('https://brew-crew-backend.onrender.com/dbResults').then(response => {
+    console.log(response);
+  });
+  
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
+      <>
+        <div>
+          <img src={ user.picture } alt={ user.name } />
+          <h2>{ user.name }</h2>
+          <p>{ user.email }</p>
+        </div>
+
+        <div>
+          ROUTE DATA HERE...
+        </div>
+      </>
     )
   );
 };
